a8aedc90b83fdf47755c9c66c3de5448772bb322
rdale240 <33500259+rdale240@users.noreply.github.com>
2018-09-27 08:38:54 -0400

    Add files via upload

--- chronic_kidney_reader.py
+++ chronic_kidney_reader.py
@@ -3,6 +3,7 @@ import os
 import inspect
 import csv
 import math
+from random import shuffle
 
 class ckd:
     def __init__ (self, age,bp,sg,al,su,rbc,pc,pcc,ba,bgr,bu,sc,sod,pot,hemo,pcv,wc,rc,htn,dm,cad,appet,pe,ane,cl):
@@ -128,18 +129,18 @@ class ckd:
     def print(self):
         print(self.attributes)
 
+class normalizedCDK:
+    def __init__ (self, attrib ,cl):
+        self.attributes = attrib
+        self.cl = (cl)
     
-def printArray(arr):
-    for a in arr:
-        print(a.attributes)
-
 def normalize(arr):
     min = [math.inf] * 25
     max = [0] * 25
     newData = []
     for a in arr:
         for index, s in enumerate(a.attributes):
-            print(s)
+            #print(s)
             if s < min[index]:
                 min[index] = s
             elif s > max[index]:
@@ -151,7 +152,7 @@ def normalize(arr):
         for index, s in enumerate(a.attributes):
             s = (s - min[index])/(max[index] - min[index])
             temp.append(s)
-        print(temp, "  |||||  ")
+        #print(temp, "  |||||  ")
         newData.append(temp)
         temp = []
     #print(newData, " ")
@@ -167,13 +168,16 @@ def normalize(arr):
             g.write(",")
         f.write("\n")
         g.write("\n")
+    f.close()
+    g.close()
+    return newData
 
 
 
 file = os.getcwd()
 filepath = os.path.join(file,"Chronic_Kidney_Disease")
 file = os.path.join(filepath, "ckd_data.csv")
-print(file)
+#print(file)
 data = []
 
 with open(file,"r") as csv_file:
@@ -181,7 +185,7 @@ with open(file,"r") as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            #print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             datum = ckd(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23], row[24])
@@ -190,4 +194,92 @@ with open(file,"r") as csv_file:
             line_count += 1
 
 #printArray(data)
-normalize(data)
+def guess_class(p, weights):
+    num = 0
+    for i in range(len(p.attributes)):
+        num += weights[i] * p.attributes[i]
+    num += weights[24]
+    #print("Class of P ", p.cl, " Guess Number ", num)
+    if num > 0:
+        if p.cl != 1.0:
+            print("False Positive")
+            #print(num, p.attributes, p.cl)
+        #print("positive")
+        return 1
+    else:
+        if p.cl != 0.0:
+            print("False Negative")
+            #print(num, p.attributes, p.cl)
+        #print("negative")
+        return 0
+
+
+def adjust_weights(p, w, learn_rate, outcome):
+    #print("Outcome", outcome, "Actual Class ", p.cl)
+    weights = w
+    if (p.cl != outcome):
+        print("Starting Weight Vector", w)
+        for i in range(len(weights) - 1):
+            weights[i] = weights[i] + (learn_rate * (p.cl - outcome))*p.attributes[i]
+        weights[23] = weights[23] + (learn_rate * (p.cl - outcome))
+        print("Ending Weight Vector", weights)
+    return weights
+
+
+def epoch(parray, w, learn_rate):
+    weights = w
+    for p in parray:
+        guess = guess_class(p, weights)
+        weights = adjust_weights(p, weights, learn_rate, guess)
+    
+    print(weights)
+
+def epochTest(parray, w, learn_rate):
+    weights = w
+    num = 0
+    for p in parray:
+        guess = guess_class(p, weights)
+        if p.cl!=guess:
+            num = num + 1
+    xlFile.write(str(num) +"," + str(learn_rate) + "\n" )
+    
+    
+    
+
+data = normalize(data)
+def normalizeCKD(data):
+    normalizedData = []
+    for d in data:
+        c = normalizedCDK(d[:24],d[24])
+        normalizedData.append(c)
+    return normalizedData
+normalizedData = normalizeCKD(data)
+
+posData = normalizedData[:250]
+negData = normalizedData[250:]
+
+testSet = []
+
+for a in posData[:50]:
+    testSet.append(a)
+for b in negData[:50]:
+    testSet.append(b)
+
+trainingSet = []
+for a in posData[50:]:
+    trainingSet.append(a)
+for b in negData[50:]:
+    trainingSet.append(b)
+
+weights = [1] * 25
+shuffle(trainingSet)
+learn_rate = .05
+xlFile = open("CKD_OUTPUT.csv", "w")
+for i in range(255):
+    epoch(trainingSet, weights, learn_rate)
+    epochTest(testSet, weights, learn_rate)
+    learn_rate = learn_rate + .05
+    weights = [1] * 25
+
+#print(trainingSet)
+#print(trainingSet[299][24])
